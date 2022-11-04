9d03d2cb77325927df349cf31f59c977f52e7073
Samanta S <samantalsingh@gmail.com>
2022-11-04 01:28:05 -0600

    styled page

--- README.md
+++ README.md
@@ -4,4 +4,9 @@
 ###### Github [Pages](https://samantasingh.github.io/cpnt262-individual-project/)
 
 # Attributions
+###### - [Starter Code](https://sait-wbdv.github.io/slides/f22/cpnt-262/js-async.html#/4)
+
+###### - [Tony - in class example](https://github.com/acidtone)
 ###### - [API](https://ghibliapi.herokuapp.com/#section/Use-Case)
+###### - [Input type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit)
+###### - [Background](https://www.pexels.com/photo/grayscale-photography-of-assorted-apparels-on-shelf-rack-1884581/)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,25 @@
-const randomInteger = function(max){
-  const randomInteger = Math.floor(Math.random() * max);
-  return randomInteger;
+const randomProduct = function(max){
+  const randomProduct = Math.floor(Math.random() * max);
+  return randomProduct;
 }
 
 const fetchData = async function() {
   const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
 
-  const product = data[randomInteger(data.length)];
+  const product = data[randomProduct(data.length)];
   const body = document.querySelector('body');
 
   output = `
-  <article>
-    <h3>${product.description}</h3>
+  <figure>
+  <h4>Category: ${product.category}</h4>
+    <h1>${product.title}</h1>
     <img src="${product.image}" alt="picture of ${product.description}">
-    <ul>
-    <li>Price:${product.price}</li>
-    <li>Rating:${product.rating}</li>
-    <li>Title:${product.title}</li>
-    <ul>
-  </article>
+    <figcaption>${product.description}</figcaption>
+    <h4>Price: $${product.price}</h4>
+  </figure>
   `
  document.getElementById("output").innerHTML = output;
 }
+
 fetchData("https://fakestoreapi.com/products")
